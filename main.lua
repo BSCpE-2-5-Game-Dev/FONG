b3bbb2430275eea6f5048f88d506071c914cbf95
Daniella Mae Alviar <daniellaalviar@gmail.com>
2020-11-07 20:36:45 +0800

    Update on Fong

--- main.lua
+++ main.lua
@@ -1,2 +1,83 @@
-#Fong
-#Let's go!
+push = require 'push'
+Class = require 'class'
+require 'Paddle'
+require 'Ball'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+PADDLE_SPEED = 250
+
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+    math.randomseed(os.time())
+    SFont = love.graphics.newFont('font.ttf', 8)
+    love.graphics.setFont(SFont)
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, 
+    {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    Player1 = Paddle(10, 30, 5, 20)
+    Player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30, 5, 20)
+    ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    gameState = 'start'
+end
+
+function love.update(dt)
+    if love.keyboard.isDown('w') then
+        Player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        Player1.dy = PADDLE_SPEED
+    else
+        Player1.dy = 0
+    end
+
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
+    if gameState == 'play' then
+        ball:update(dt)
+    end
+
+    Player1:update(dt)
+    Player2:update(dt)
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    elseif key == 'enter' or key == 'return' then
+        if GState == 'start' then
+            GState = 'play'
+        else
+            GState = 'start'
+            ball:reset()
+        end
+    end
+end
+
+function love.draw()
+    push:apply('start')
+    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
+    love.graphics.setFont(smallFont)
+    if GState == 'start' then
+        love.graphics.printf('Hello Start State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Hello Play State!', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
+
+    player1:render()
+    player2:render()
+    ball:render()
+    push:apply('end')
+end
